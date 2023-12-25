7d790609ac9a463d4d0b188cfbc4f2835ecd8ac7
Srikanthh-21 <Srikanthpaindla21@gmail.com>
2023-12-25 18:45:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var randomNumber2=Math.floor((Math.random()*6)+1);
 var randomDiceImage1="dice" + randomNumber1 + ".png";
 var randomDiceImage2="dice" + randomNumber2 + ".png";
 
-var randomImageSource1="./images/" + randomDiceImage1;
-var randomImageSource2="./images/" + randomDiceImage2;
+var randomImageSource1="images/" + randomDiceImage1;
+var randomImageSource2="images/" + randomDiceImage2;
 
 var image1=document.querySelector(".img1");
 
